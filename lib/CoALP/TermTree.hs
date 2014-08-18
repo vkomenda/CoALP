251f77db3c052b0e5d0411401e321060784e9a36
Vladimir Komendantsky <komendantsky@gmail.com>
2014-08-18 08:10:39 +0100

    isTermTree

--- lib/CoALP/TermTree.hs
+++ lib/CoALP/TermTree.hs
@@ -17,7 +17,7 @@ import Data.Set (Set, member)
 import Data.Map (Map, keysSet, (!))
 --import qualified Data.Map as Map
 import Data.Foldable
-import Data.Sequence (ViewL(..), viewl, length, drop, (|>))
+import Data.Sequence ((|>))
        -- FIXME: define operations on underlying types in TreeLang
 
 type Symbol = String
@@ -40,18 +40,11 @@ isTermTree (TermTree sg l t) =
   isSig sg && isTreeLang l &&
   keysSet t == l && all respectArity l
   where
-    noChildren w =  w \: l == singleton
     respectArity w = case t!w of
-      Right _ -> noChildren w
+      Right _ -> not $ (w |> 0) `member` l
       Left a  -> a `member` symbols sg &&
                  let ar = arity sg ! a in
                  -- sufficient condition
-                 (w |> ar - 1) `member` l &&
+                 if (ar > 0) then (w |> ar - 1) `member` l else True &&
                  -- necessary condition
-                 all (\u -> if w `isPrefix` u then
-                              case viewl $ drop (length w) u of
-                                EmptyL -> True
-                                i :< _ -> i < ar
-                            else
-                              True
-                     ) l
+                 not ((w |> ar) `member` l)
